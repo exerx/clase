aeaaed93ea60166ee61994e1ea662086b961a8c2
Ever <exer.die@gmail.com>
2025-05-24 17:52:16 -0600

    Update tests.py
    Hols

--- tests.py
+++ tests.py
@@ -2,5 +2,5 @@ import pytest
 from main import Operaciones
 
 def test_suma():
-    assert Operaciones.suma(5, 6) == 11, "5 + 6 debería ser 11"
+    assert Operaciones.suma(5, 6) == 11, "5 + 6 debería 11"
     assert Operaciones.suma(-2, 4) == 2, "-2 + 4 debería ser 2"
